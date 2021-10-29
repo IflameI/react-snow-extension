f48790c87e64e134477bba851128a27262b05a81
iflamei <iflamei@mail.ru>
2021-10-29 15:21:26 +0300

    Final Vesrion 1.0.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+const App = () => {
+  return <Button />;
+};
+
+export default App;
